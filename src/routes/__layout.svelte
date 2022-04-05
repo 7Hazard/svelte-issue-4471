f8406569be6ff9ae7f83d75d45ba3072cf19b11d
Hazard <hazard9909@gmail.com>
2022-04-05 13:59:59 +0200

    Initial commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,86 @@
+<script lang="ts">
+</script>
+
+<!-- DEFINING HTML IN svelte:head BREAKS CSS -->
+<svelte:head>
+	<html lang="en" />
+</svelte:head>
+
+<slot />
+
+<style global>
+	@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400&display=swap');
+
+	:root {
+		--color1: #00b9ae;
+		--color2: #009f93;
+		--color3: #037979;
+		--color4: #292929;
+		--color5: #303030;
+		--color6: rgb(59, 59, 59);
+		--color7: rgb(77, 77, 77);
+		--color8: rgb(73, 73, 73);
+		--input-color1: var(--color6);
+		--input-color2: var(--color7);
+		--input-color3: var(--color8);
+
+		font-family: 'Montserrat', sans-serif;
+		color: rgba(255, 255, 255, 0.974);
+	}
+
+	body {
+		background-color: var(--color3);
+		margin: 0;
+		padding: 0.6em 1.5em;
+		width: 100%;
+		box-sizing: border-box;
+	}
+
+	@media only screen and (max-width: 768px) {
+		body {
+			padding: 0em 0.4em;
+		}
+	}
+
+	ul {
+		padding: 0;
+		margin: 0;
+	}
+
+	input {
+		background-color: var(--input-color1);
+		color: rgba(255, 255, 255, 0.967);
+		border: solid;
+		border-width: 2px;
+		border-color: var(--color4);
+		border-radius: 3px;
+	}
+	::placeholder {
+		color: rgba(255, 255, 255, 0.542);
+	}
+
+	input {
+		padding: 0.5em;
+		font-size: medium;
+	}
+
+	input[type='submit']:hover {
+		cursor: pointer;
+		background-color: var(--input-color2);
+	}
+
+	input[type='submit']:active {
+		background-color: var(--input-color3);
+	}
+
+	textarea {
+		background-color: var(--input-color1);
+		color: rgba(255, 255, 255, 0.967);
+		border: solid;
+		border-width: 2px;
+		border-color: rgba(38, 38, 38, 0.83);
+		border-radius: 3px;
+		font-size: large;
+		box-sizing: border-box;
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { fly } from 'svelte/transition';
+
+	let email: string;
+	let password: string;
+	function signin(ev: SubmitEvent) {
+		ev.preventDefault();
+		goto("/timereport")
+	}
+</script>
+
+<svelte:head>
+	<!-- <html lang="en" /> -->
+	<title>Login</title>
+</svelte:head>
+
+<form on:submit={signin} in:fly={{ y: -10, duration: 700 }}>
+	<h1>Login</h1>
+	<input type="text" placeholder="Email" bind:value={email} />
+	<input type="password" placeholder="Password" bind:value={password} />
+	<input type="submit" value="Sign In" style="margin-top: 0.6em" />
+</form>
+
+<style>
+	form {
+		background-color: var(--color5);
+
+		padding: 1em 2em 2em 2em;
+		margin: 5vh auto;
+		width: fit-content;
+
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		gap: 0.1em;
+
+		border-radius: 4px;
+	}
+</style>
